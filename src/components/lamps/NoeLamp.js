41e4540b72ffd4584b506a4bccc64a9016252dc9
Angela Loh <angelaloh09@gmail.com>
2026-02-26 09:09:10 -0800

    noe lamp description + photos

--- src/components/lamps/NoeLamp.js
+++ src/components/lamps/NoeLamp.js
@@ -1,23 +1,50 @@
 import React from 'react'
 import Navbar from '../Navbar'
 import '../styles/Styles.css'
-import style from '../styles/aboutme.module.css'
+import style from '../styles/noe-lamp.module.css'
+
+import img1 from './noe-lamp/IMG_2088.jpg'
+import img2 from './noe-lamp/DSCF4702.jpg'
+import img3 from './noe-lamp/DSCF4680.jpg'
+
+const images = [img2, img3, img1]
 
 const NoeLamp = () => {
     return (
-        <section className='ideas'>
+        <section className={`ideas ${style.noeLampSection}`}>
             <Navbar/>
-            <div className={style.PagePadding}>
-                <div className="cards title">
-                    <h1>Noe Lamp</h1>
-                    <br></br>
+            <div className={style.contentWrapper}>
+            <div className={style.container}>
+                <div className={style.layout}>
+                    {/* Left column - images */}
+                    <div className={style.imageColumn}>
+                        {images.map((img, idx) => (
+                            <img
+                                key={idx}
+                                src={img}
+                                alt={`Noe Lamp view ${idx + 1}`}
+                                className={idx < 2 ? style.largeImage : style.bottomImage}
+                            />
+                        ))}
+                    </div>
+
+                    {/* Right column - text */}
+                    <div className={style.textColumn}>
+                        <h1 className={style.title}>Noe Lamp</h1>
+                        <div className={style.description}>
+                            <p>
+                                A 3D printed lamp with warm, aesthetic lighting. Designed for cozy corners.
+                            </p>
+                            <p>
+                                The Noe Lamp is named after a cute street in San Francisco I used to live on. 
+                                This lamp should feel cozy, warm, and inviting - just like Noe Street! 
+                                I designed this lamp as a gift for my Noe Street roommates and to capture what it felt like to live on Noe Street.
+
+                            </p>
+                        </div>
+                    </div>
                 </div>
-                
-                <div className={style.Writing}>
-                    <p>
-                        A 3D printed lamp with warm, aesthetic lighting.
-                    </p>
-                </div>
+            </div>
             </div>
         </section>
     )
