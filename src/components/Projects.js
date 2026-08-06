8cb5a27079eb5b5e8273154541f902472c97ac53
Angela Loh <angelaloh09@gmail.com>
2026-08-06 11:01:31 -0700

    project page

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -3,32 +3,40 @@ import Navbar from './Navbar'
 import './styles/Styles.css'
 import style from './styles/projects.module.css'
 import { Link } from 'react-router-dom'
-import noeLampImg from './tinkio-projects/noe-lamp/noe-lamp-img1.jpg'
+import noeLampImg from './tinkio-projects/noe-lamp/noe-lamp-img2.jpg'
+import noeLampHoverImg from './tinkio-projects/noe-lamp/noe-lamp-img3.jpg'
 import cloudVaseImg from './tinkio-projects/cloud-vase/DSCF4753.jpg'
+import cloudVaseHoverImg from './tinkio-projects/cloud-vase/img1.jpg'
 import vimputerImg from './tinkio-projects/vimputer/DSCF6306.jpg'
+import vimputerHoverImg from './tinkio-projects/vimputer/typewriter.jpeg'
 import enzoStoolImg from './tinkio-projects/enzo-stool/DSCF6369.jpg'
+import enzoStoolHoverImg from './tinkio-projects/enzo-stool/DSCF6350.jpg'
 
 const projects = [
   {
     title: 'Noe Lamp',
     path: '/tinkio-projects/noe-lamp',
     image: noeLampImg,
-  },
-  {
-    title: 'Cloud Vase',
-    path: '/tinkio-projects/cloud-vase',
-    image: cloudVaseImg,
+    hoverImage: noeLampHoverImg,
   },
   {
     title: 'Vimputer',
     path: '/tinkio-projects/vimputer',
     image: vimputerImg,
+    hoverImage: vimputerHoverImg,
     objectPosition: 'center top',
+  },
+  {
+    title: 'Cloud Vase',
+    path: '/tinkio-projects/cloud-vase',
+    image: cloudVaseImg,
+    hoverImage: cloudVaseHoverImg,
   },
   {
     title: 'Enzo Stool',
     path: '/tinkio-projects/enzo-stool',
     image: enzoStoolImg,
+    hoverImage: enzoStoolHoverImg,
   },
 ]
 
@@ -51,12 +59,22 @@ const Projects = () => {
             <Link key={project.path} to={project.path} className={style.card}>
               <div className={style.cardImageWrapper}>
                 {project.image ? (
-                  <img
-                    src={project.image}
-                    alt={project.title}
-                    className={style.cardImage}
-                    style={project.objectPosition ? { objectPosition: project.objectPosition } : undefined}
-                  />
+                  <>
+                    <img
+                      src={project.image}
+                      alt={project.title}
+                      className={style.cardImage}
+                      style={project.objectPosition ? { objectPosition: project.objectPosition } : undefined}
+                    />
+                    {project.hoverImage ? (
+                      <img
+                        src={project.hoverImage}
+                        alt=""
+                        aria-hidden="true"
+                        className={`${style.cardImage} ${style.cardImageHover}`}
+                      />
+                    ) : null}
+                  </>
                 ) : (
                   <div
                     className={style.cardImagePlaceholder}
